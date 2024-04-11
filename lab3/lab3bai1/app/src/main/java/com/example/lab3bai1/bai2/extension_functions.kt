ea9af7060f33c30d9fb9c59733d2366bd9cf97d5
ThongThao <thaotran4135@gmail.com>
2024-04-11 21:36:36 +0700

    add

---
+++ lab3/lab3bai1/app/src/main/java/com/example/lab3bai1/bai2/extension_functions.kt
@@ -0,0 +1,16 @@
+package com.example.lab3bai1.bai2
+
+
+open class AquariumPlant(val color: String, private val size: Int)
+
+class GreenLeafyPlant(size: Int) : AquariumPlant("green", size)
+
+fun AquariumPlant.print() = println("AquariumPlant")
+fun GreenLeafyPlant.print() = println("GreenLeafyPlant")
+
+fun main(){
+    val plant = GreenLeafyPlant(size = 10)
+    plant.print()
+    val aquariumPlant: AquariumPlant = plant
+    aquariumPlant.print()
+}
